fa79d0337160f6672ebcc28e6d57119fb5c995b5
MykulCoding <Marm2@github.com>
2024-01-29 20:49:29 +0000

    Installed Vite, Installed Bootstrap, updated App.jsx file by adding bootstrap import and returning (this is my portfolio) as the app function, Installed react router, updated vite.config.js file

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap'; // Import all of Bootstrap's JS
+
+
+function App() {
+  return <div>This is my portfolio</div>
+
+}
+
+export default App
