59fa5d002e90d08fb9df20b4aed7885b25b694ef
MykulCoding <Marm2@github.com>
2024-01-30 22:33:16 +0000

    added Contact.jsx codes

---
+++ portfolio/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+
+function Contact() {
+    return <div className='background min-vh-100 pb-5'>
+      
+    </div>
+}
+export default Contact
