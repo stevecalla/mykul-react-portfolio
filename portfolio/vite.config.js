c5fbc6e0c2a2f8f9cdb38d2870d537721bd2dcb4
MykulCoding <Marm2@github.com>
2024-01-30 21:16:02 +0000

    Due to errors, re-cloned repo and re-added sub folders inside the src (assets, component, data & pages)

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,10 +1,11 @@
+// Importing necessary functions and plugins from Vite and React
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Vite configuration exported as a default function
 // https://vitejs.dev/config/
 export default defineConfig({
+// Using the 'react' plugin for Vite to enable React support
   plugins: [react()],
-  build: {
-    outDir: '../'
-  }
 })
