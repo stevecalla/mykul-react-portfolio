cc989097ede99ecacbd30ce03d42cbeb1ede55a7
MykulCoding <Marm2@github.com>
2024-01-30 21:53:27 +0000

    added code to app.jsx file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap'; // Import all of Bootstrap's JS
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+import NavBar from './component/NavBar';
+import Footer from './component/Footer';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 
 function App() {
-  return <div>This is my portfolio</div>
-
+    return <Router>
+        <NavBar />
+        <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer/>
+    </Router>;
 }
-
 export default App
