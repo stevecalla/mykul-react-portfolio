b7e9de70ca0d3451d482cfa5bce87301b0dd4cc1
Steve Calla <callasteven@gmail.com>
2024-01-31 15:44:54 -0700

    Update

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,35 +1,55 @@
 // Importing custom styles for the Home page
-import './PageStyles.css';
+import "./PageStyles.css";
 
 // import headshotImage from '../asset/headshot.jpg';
-import headshot from "../../public/assets/headshot.jpg"
+import headshot from "../../public/assets/headshot.jpg";
 
 // Functional component for the Home page
 function Home() {
-    return (
-        <div className='background min-vh-100 pb-5 m-0'>
-            <div className='row g-0 p-5 align-items-center'>
-                <div className='col col-lg-4 p-4'>
-                <img src="./assets/headshot.jpg" alt='Profile Picture' className='img-fluid' />
-                </div>
-
-                <div className='col col-lg-7'>
-                    <h2 className="p-5">My name is Michael Adjei...</h2>
-                    <h5 className="py-3 p-5">
-                        An aspiring front-end web designer, I am deeply passionate about creating immersive and user-friendly digital experiences. My dedication is reflected in the meticulous process of crafting visually appealing and highly functional websites that not only meet but exceed user expectations.
-                        <br />
-                        <br />
-                        Driven by a relentless pursuit of excellence, I draw inspiration from the ever-evolving landscape of web development. Armed with a solid foundation and continuous learning mindset, I have honed my skills through a comprehensive edx web development certification program. This journey has equipped me with the latest industry trends, best practices, and cutting-edge technologies.
-                        <br />
-                        <br />
-                        Seeking opportunities in the fields of Web Development and Testing. Please review my CV 
-                        {/* <a href='portfolio\src\assets\MA_CV (1).pdf' target="_blank" rel="noopener noreferrer">here</a> */}
-                        <a href='./assets/MA_CV.pdf' target="_blank" rel="noopener noreferrer">here</a>
-                    </h5>
-                </div>
-            </div>
+  return (
+    <div className="background min-vh-100 pb-5 m-0">
+      <div className="row g-0 p-5 align-items-center">
+        <div className="col col-lg-4 p-4">
+          <img
+            src="./assets/headshot.jpg"
+            alt="Profile Picture"
+            className="img-fluid"
+          />
         </div>
-    );
+
+        <div className="col col-lg-7">
+          <h2 className="p-5">My name is Michael Adjei...</h2>
+          <h5 className="py-3 p-5">
+            An aspiring front-end web designer, I am deeply passionate about
+            creating immersive and user-friendly digital experiences. My
+            dedication is reflected in the meticulous process of crafting
+            visually appealing and highly functional websites that not only meet
+            but exceed user expectations.
+            <br />
+            <br />
+            Driven by a relentless pursuit of excellence, I draw inspiration
+            from the ever-evolving landscape of web development. Armed with a
+            solid foundation and continuous learning mindset, I have honed my
+            skills through a comprehensive edx web development certification
+            program. This journey has equipped me with the latest industry
+            trends, best practices, and cutting-edge technologies.
+            <br />
+            <br />
+            Seeking opportunities in the fields of Web Development and Testing.
+            Please review my CV
+            {/* <a href='portfolio\src\assets\MA_CV (1).pdf' target="_blank" rel="noopener noreferrer">here</a> */}
+            <a
+              href="./assets/MA_CV.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 // Export the Home component
